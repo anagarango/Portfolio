4b3839f853d576823874150a467003630b792011
Ana Arango <anaarango@Anas-Macbook-Air.local>
2023-02-28 01:09:50 -0800

    updated hero section for mobile responsiveness

--- components/menu.js
+++ components/menu.js
@@ -5,10 +5,10 @@ export default function Menu(){
     return(
         <Container height="100vh" position="fixed" left="0" top="0" flexDir="column">
             <Box width="70px" height="95%" bgColor="#28284D" flexDir="column" aliIt="center">
-                <Image src="/Icons/CollaboratorFemale.svg" width={50} height={50} style={{display:"flex", margin:"25px 0px"}}></Image>
-                <Image src="/Icons/Albums.svg" width={50} height={50} style={{display:"flex", marginBottom:"25px"}}></Image>
-                <Image src="/Icons/SourceCode.svg" width={50} height={50} style={{display:"flex", marginBottom:"25px"}}></Image>
-                <Image src="/Icons/Resume.svg" width={50} height={50} style={{display:"flex", position:"absolute", bottom:0, marginBottom:"5%"}}></Image>
+                <Image src="/Icons/CollaboratorFemale.svg" width={40} height={40} style={{display:"flex", margin:"30px 0px"}}></Image>
+                <Image src="/Icons/Albums.svg" width={40} height={40} style={{display:"flex", marginBottom:"30px"}}></Image>
+                <Image src="/Icons/SourceCode.svg" width={40} height={40} style={{display:"flex", marginBottom:"30px"}}></Image>
+                <Image src="/Icons/Resume.svg" width={40} height={40} style={{display:"flex", position:"absolute", bottom:0, marginBottom:"5%"}}></Image>
             </Box>
             <Box height="5%" bgColor="#1E1E41" aliIt="center" padding="0 20px" justCont="space-between">
                 <H4 color="#9D95DC">Intro</H4>
