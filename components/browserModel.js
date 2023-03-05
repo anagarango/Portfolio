8606f2f31910a6687f5cda8147b174f7ef92000c
Ana Arango <anaarango@Anas-Macbook-Air.local>
2023-03-04 16:18:49 -0800

    updated browser, footer, header, slideshow

--- components/browserModel.js
+++ components/browserModel.js
@@ -1,4 +1,4 @@
-import { Box, Hero, Text } from "./globals"
+import { Container, Box, Hero, Text, H1 } from "./globals"
 import { motion, AnimatePresence } from "framer-motion";
 
 export default function BrowserModel({
@@ -7,22 +7,22 @@ export default function BrowserModel({
     onClick=()=>{},
 }){
     return(
-        <Box width="90%" minHeight="200px" margin="0 50px" border="1px solid grey" borderRadius="10px" flexDir="column">
-        <Hero src="/SVG/Browser.svg" bgSize="cover" bgRepeat="no-repeat" width="100%" height="45px">
-          <Box width="100%" height="100%" margin="0 0 0 100px" aliIt="center" style={{overflowX: "scroll", whiteSpace: "nowrap"}}>
-            {array.map((o,i) => (
-              <Text value={i} onClick={onClick} key={i} cursor="pointer" aliIt="center" height="fit-content" minWidth="fit-content" padding="5px 10px" bgColor={array[state].title === o.title ? "#A399E2" : "#28284D"} style={{transition: "background-color 0.5s ease-out"}} borderRadius="6px" margin="0 10px 0 0">{o.title}</Text>
-            ))}
-          </Box>
-        </Hero>
-        <AnimatePresence mode='wait'>
-                <motion.div key={array[state] ? array[state].title : "empty"} initial={{ opacity: 0, display:"none" }} animate={{ opacity: 1, display:"flex"}} exit={{ opacity: 0, display:"none"}} transition={{ duration: 1}} style={{display:"flex", flexDirection:"column", padding:"25px", transition: "0.5s ease-out"}}>
-                  <Text fontWeight="700" padding="0 0 15px 0">{state.title}</Text>
-                  <Text padding="0 0 10px 0">{array[state].p1}</Text>
-                  <Text padding="0 0 10px 0">{array[state].p2}</Text>
-                  <p style={{color:"white"}}><strong style={{fontWeight:"800", color:"#EA638D"}}>{array[state].projectName}</strong>{array[state].project}</p>
-                </motion.div>
-            </AnimatePresence>
-      </Box>
+        <Box width="100%" minHeight="250px" border="1px solid grey" borderRadius="10px" flexDir="column">
+          <Hero src="/SVG/Browser.svg" bgSize="cover" bgRepeat="no-repeat" width="100%" height="45px">
+            <Box width="100%" height="100%" margin="0 0 0 100px" aliIt="center" style={{overflowX: "scroll", whiteSpace: "nowrap"}}>
+              {array.map((o,i) => (
+                <Text value={i} onClick={onClick} key={i} cursor="pointer" aliIt="center" height="fit-content" minWidth="fit-content" padding="5px 10px" bgColor={array[state].title === o.title ? "#A399E2" : "#28284D"} style={{transition: "background-color 0.5s ease-out"}} borderRadius="6px" margin="0 10px 0 0">{o.title}</Text>
+              ))}
+            </Box>
+          </Hero>
+          <AnimatePresence mode='wait'>
+            <motion.div key={array[state] ? array[state].title : "empty"} initial={{ opacity: 0, display:"none" }} animate={{ opacity: 1, display:"flex"}} exit={{ opacity: 0, display:"none"}} transition={{ duration: 1}} style={{display:"flex", flexDirection:"column", padding:"25px", transition: "0.5s ease-out"}}>
+              <Text fontWeight="700" padding="0 0 15px 0">{array[state].title}</Text>
+              <Text padding="0 0 10px 0">{array[state].p1}</Text>
+              <Text padding="0 0 10px 0">{array[state].p2}</Text>
+              <p style={{color:"white"}}><strong style={{fontWeight:"800", color:"#EA638D"}}>{array[state].projectName}</strong>{array[state].project}</p>
+            </motion.div>
+          </AnimatePresence>
+        </Box>
     )
 }
--- components/slideshow.js
+++ components/slideshow.js
@@ -1,20 +1,20 @@
 import { Tech } from "@/data/tools"
-import Image from "next/image"
-import { Box, Text } from "./globals"
+// import Image from "next/image"
+import { Box, Text, Image } from "./globals"
 
 export default function TechSlideshow(){
     return(
         <Box style={{position:"relative", overflow:"hidden", background:"linear-gradient(to right, rgba(163,153,226,0) 20%, rgba(163,153,226,1) 50%,rgba(163,153,226,0) 80%)", width:"115%", margin:"0 -40px"}}>
             <Box className='sheesh' style={{display:"flex", position:"relative", alignItems:"flex-start", justifyContent:"flex-start", padding:"10px 10px 5px 10px"}}>
-              {Tech.map((o,i)=>(<Box flexDir="column" aliIt="center" width="145px">
-                  <Image className="techLogo" src={`/HorizontalLogos/${o.src}`} alt={o.title} width={60} height={60}></Image>
-                  <Text textAlign="center" padding="15px 0 0 0">{o.title}</Text>
-                </Box>
+              {Tech.map((o,i)=>(
+              // <Box flexDir="column" aliIt="center" width="145px">
+                <Image title={o.title} className="techLogo" src={`/HorizontalLogos/${o.src}`} alt={o.title} width={45} height={45} margin="0 20px"></Image>
+              // </Box>
               ))}
-              {Tech.map((o,i)=>(<Box flexDir="column" aliIt="center" width="145px">
-                  <Image className="techLogo" src={`/HorizontalLogos/${o.src}`} alt={o.title} width={60} height={60}></Image>
-                  <Text textAlign="center" padding="15px 0 0 0">{o.title}</Text>
-                </Box>
+              {Tech.map((o,i)=>(
+                // <Box flexDir="column" aliIt="center" width="145px">
+                  <Image title={o.title} className="techLogo" src={`/HorizontalLogos/${o.src}`} alt={o.title} width={45} height={45} margin="0 20px"></Image>
+                // </Box>
               ))}
             </Box>
         </Box>
