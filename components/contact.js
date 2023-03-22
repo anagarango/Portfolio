b3bc449357aeb26d9252515d5c7d99f5e464f895
Ana Arango <anaarango@Anas-Macbook-Air.local>
2023-03-21 23:12:46 -0700

    contact form

--- components/contact.js
+++ components/contact.js
@@ -1,33 +1,49 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
-import { Box, Form, Text } from './globals';
+import { Box, Form, Text, Image } from './globals';
 import Inputs from './input';
-// const publicKey = process.env.public_key 
+import { motion, AnimatePresence } from "framer-motion"
 
-export default function Contact({apiKey}){
+
+export default function Contact({
+  PUBLICkey
+}){
   const form = useRef();
-  console.log(apiKey)
+  const [message, setMessage] = useState(false)
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_7fufcnn', 'template_vs93m8o', form.current, apiKey)
+    emailjs.sendForm('service_7fufcnn', 'template_vs93m8o', form.current, PUBLICkey)
       .then((result) => {
           console.log(result.text);
+          form.current.reset();
+          setMessage(true)
+          setTimeout(() => {
+            setMessage(false)
+          }, 6000);
       }, (error) => {
           console.log(error.text);
       });
   };
 
+
+
   return (
     <Form ref={form} onSubmit={sendEmail}>
+      {message &&  <AnimatePresence exitBeforeEnter>
+            <motion.div  initial={{ x: 400 }} animate={{ x: 0, transition: { duration: 1, delay: 0 }}} exit={{ x: 400 }} style={{display:"flex", position:"fixed", zIndex:"15", left:"85vw", top:"100px", padding:"15px 30px", backgroundColor:"#4BDA4B", alignItems:"center", width:"fit-content"}}> 
+                <Image src="/check.png" width={40} height={40} alt="checkmark" margin="0 20px 0 0"/>
+                <Text padding="0px" color='white' fontWeight="600">Message Sent!</Text>
+            </motion.div>
+        </AnimatePresence>}
         <Box flexDir="column" width="100%" maxWidth="400px" padding="15px 0px">
             <Text fontWeight="700">Name</Text>
-            <Inputs type="text" name="user_name" placeholder="First and Last Name" />
+            <Inputs type="text" name="from_name" placeholder="First and Last Name" />
         </Box>
         <Box flexDir="column" width="100%" maxWidth="400px">
             <Text fontWeight="700">Email</Text>
-            <Inputs type="email" name="user_email" placeholder="example@address.com" />
+            <Inputs type="email" name="from_email" placeholder="example@address.com" />
         </Box>
         <Box flexDir="column" width="100%" maxWidth="600px" padding="15px 0px">
             <Text fontWeight="700">Message</Text>
@@ -37,15 +53,3 @@ export default function Contact({apiKey}){
     </Form>
   );
 };
-
-export async function getServerSideProps(){
-
-    console.log(process.env.PRIVATE_API_KEY)
-  
-    return {
-      props: {
-        // apiKey: api_key
-        hello: 'world'
-      }
-    }
-  }
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useState, useRef } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { motion, AnimatePresence } from "framer-motion";
 import Header from '@/components/header'
 import Footer from '@/components/footer'
-import { Box, Hero, Container, Heading, Text, CTA, Tag } from '@/components/globals'
-import { Skills } from '@/data/skills'
-import { motion, AnimatePresence } from "framer-motion";
+import { Box, Hero, Container, Heading, Text, CTA } from '@/components/globals'
 import BrowserModel from '@/components/browserModel'
-import { Projects } from '@/data/projects'
 import TechSlideshow from '@/components/slideshow'
-import { useRouter } from 'next/router'
 import Card from '@/components/card'
 import Contact from '@/components/contact'
+import { Projects } from '@/data/projects'
+import { Skills } from '@/data/skills'
 
 export default function Home() {
   const [skill, setSkill] = useState(0)
 
   const r = useRouter()
   const currentYear = new Date().getFullYear();
-
 
   function SelectProject(data){
       r.replace({
@@ -52,22 +52,24 @@ export default function Home() {
       <main>
         <Header/>
         <Footer/>
-          <Hero id="Home" className="hero" minHeight="100vh" bgPosition="bottom left" src='/Landing.svg' bgRepeat="no-repeat" bgSize="cover" flexDir="row" padding="0px 150px" justCont="center" aliIt="center" >
+          <Hero id="Home" className="hero" minHeight="100vh" bgPosition="bottom left" src='/Landing.svg' bgRepeat="no-repeat" bgSize="cover" flexDir="row" padding="0px 150px" justCont="center" aliIt="center" priority>
             <Box className="heroCont" flexDir="column" width="50%" minHeight="fit-content" padding="0 45px 0 0">
               <Heading color="#B23C87" fontFamily='Staatliches' padding="10px 0" fSize="25px">FRONT-END DEVELOPER</Heading>
               <Heading color='#EA638D' fSize="65px" padding="10px 0">Ana Arango</Heading>
               <Text padding="50px 0">A passionate Front-End Developer with a mission to find newer challenges in improving amazing user experiences optimization and visually-appealing websites and web-applications.</Text>
-              <CTA>Don't Be A Stranger</CTA>
+              <Link href="/#Experience" scroll={false}>
+                <CTA>Don't Be A Stranger</CTA>
+              </Link>  
             </Box>
             <Box className="heroCont" width="50%">
-              <Image alt="gif of Ana Arango making faces" src="/Hero.gif" width={150} height={100} style={{width:"100%", height:"fit-content"}}></Image>
+              <Image alt="gif of Ana Arango making faces" src="/Hero.gif" width={150} height={100} style={{width:"100%", height:"fit-content"}} priority></Image>
             </Box>
           </Hero>
 
           <Container id="About" flexDir="column" width="100%" padding="10px 150px" aliIt="flex-end">
             <Heading color='#B23C87' fontFamily='Staatliches' padding="10px 0">About Me</Heading>
             <Box display="inline" width="100%">
-                <Image className='aboutmeimage' src="/aboutme.png" alt="Eagle eye shot of Ana Arango working on her desktop and laptop" width={200} height={200} style={{width:"40%", height:"100%", float:"left", margin:"0px 30px 30px 50px", minWidth:"350px"}}/>
+                <Image className='aboutmeimage' src="/aboutme.png" alt="Eagle eye shot of Ana Arango working on her desktop and laptop" width={200} height={200} style={{width:"40%", height:"100%", float:"left", margin:"0px 30px 30px 50px", minWidth:"350px"}} priority/>
                 <Text width="100%" textAlign="end" padding="0 0 30px 0">I’m Ana Arango, {currentYear-2004} year old front-end developer, from Vancouver, Canada.</Text>
                 <Text width="100%" textAlign="end" padding="0 0 30px 0">From straight of high school, I started my post-secondary education in the Digital Design and Development with no prior knowledge of programming, but after 2 years, I’ve learned many new technologies a front-end developer needs to create functional appealing websites and web-applications.</Text>
                 <Text width="100%" textAlign="end" padding="0 0 30px 0">Fueled by new ideas, I enjoy designing coding things from scratch as it continues putting new challenges on myself. I’m passionate, expressive, and naturally curious in exploring new techniques and technologies to keep up with the ever-evolving web development landscape.</Text>
@@ -86,33 +88,31 @@ export default function Home() {
 
           <TechSlideshow />
 
-          <Container id="Projects" flexDir="column" width="100%" padding="200px 150px">
+          <Container id="Projects" flexDir="column" width="100%" padding="200px 150px 150px 150px">
             <Heading color='#B23C87' fontFamily='Staatliches' padding="10px 0">Projects</Heading>
             <Text>These are a few selected projects that I believe show what languages and modern practices I have applied to create easy-to-use and and modern web applications. Feel free to explore the links I attached on each post to explore the web application’s yourself!</Text>
             {Projects.map((o,i)=>{
               if(i % 2 == 0){
                 return(
-                  <Card key={i} alt={o.name} src={o.image} heading={o.name} preview={o.preview} tech={o.tech} onClick={()=>{SelectProject(o)}} />
+                  <Card key={`a${i}`} alt={o.name} src={o.image} heading={o.name} preview={o.preview} tech={o.tech} onClick={()=>{SelectProject(o)}} />
                 )
               } else {
                 return(
-                  <Card key={i} type="right" alt={o.name} src={o.image} heading={o.name} preview={o.preview} tech={o.tech} onClick={()=>{SelectProject(o)}} />
+                  <Card key={`b${i}`} type="right" alt={o.name} src={o.image} heading={o.name} preview={o.preview} tech={o.tech} onClick={()=>{SelectProject(o)}} />
                 )
               }
               
             })}
-          
           </Container>
 
-          <Container id="Projects" flexDir="column" width="100%" padding="0px 150px 100px 150px">
+          <Container id="Experience" flexDir="column" width="100%" padding="0px 150px 100px 150px">
             <Heading color='#B23C87' fontFamily='Staatliches' padding="10px 0">Wanna Talk?</Heading>
             <Text padding="0 0 50px 0">If you're interested in working on a project together or just wanna reach out to me, fill out the form below. <strong>Ttyl!</strong></Text>
             <Box width="100%" minHeight="250px" border="1px solid grey" borderRadius="10px" flexDir="column">
               <Hero src="/SVG/Browser.svg" bgSize="cover" bgRepeat="no-repeat" width="100%" height="45px" />
-              <Contact/>
+              <Contact PUBLICkey={process.env.NEXT_PUBLIC_PRIVATE_API_KEY} />
             </Box>
           </Container>
-
           
 
       </main>
