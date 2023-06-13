ab472434f3f20ded78f2fc9b8248046549a76a4c
Ana Arango <anaarango@Anas-Macbook-Air.local>
2023-06-13 04:04:55 -0700

    fixed browser model

--- pages/[project].js
+++ pages/[project].js
@@ -12,15 +12,17 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Autoplay, Pagination, Navigation } from 'swiper';
 import ProjectList from "@/data/project-list.json"
+import BrowserModel from '@/components/browserModel'
 
 export default function Project(){
     const r = useRouter()
     const [project, setProject] = useState([])
+    const [skill, setSkill] = useState(0)
+        
 
 
     useEffect(()=>{
         const url = window.location.pathname.split('/').pop();
-
         for(var x = 0; x < ProjectList.length; x++){
             if(r.query.project == ProjectList[x].name || url == ProjectList[x].name){
                 setProject(ProjectList[x])
@@ -42,12 +44,12 @@ export default function Project(){
         <Header/>
         <Footer/>
 
-          <Container id="About" flexDir="column" width="100%" padding="120px" aliIt="flex-start">
-            <Hero src="/blob.svg" minHeight="85vh" width="100%" bgSize="contain" bgRepeat="no-repeat" aliIt="center" justCont="center" flexDir="column">
+          <Container flexDir="column" width="100%" padding="150px" aliIt="flex-start">
+            <Hero src="/blob.svg" bgPosition="center" minHeight="75vh" width="100%" bgSize="contain" bgRepeat="no-repeat" aliIt="center" justCont="center" flexDir="column">
                 <Heading color='#9DFFFF' fSize="70px" margin="-125px 0 -20px 0" fontFamily='Staatliches'>{project.name}</Heading>
                 <Image alt={project.preview} src={project.image} width={600} height={300} priority style={{width:"75%", height:"45%", borderRadius:"20px", boxShadow:"0px 7px 45px 1px #000000"}} />
             </Hero>
-            <Box padding="0px 30px" flexDir="column">
+            <Box padding="100px 0 0 0" flexDir="column">
                 <Heading color='#9DFFFF' fontFamily='Staatliches' fSize="22px" width="100%" padding="60px 0 0 0">Project Overview</Heading>
                 <Text padding="20px 0 0 0">{project.projectOverview}</Text>
                 <Heading color='#9DFFFF' fontFamily='Staatliches' fSize="22px" width="100%" padding="60px 0 0 0">Role</Heading>
@@ -59,10 +61,17 @@ export default function Project(){
                         <TechCard key={i} src={o} alt={o} heading={o} />
                     ))}
                 </Box>
+
+                <Heading color='#9DFFFF' fontFamily='Staatliches' fSize="22px" width="100%" padding="60px 0 20px 0">Key Features</Heading>
+                <BrowserModel version='project' onClick={(event)=>{setSkill(event.target.getAttribute("value"))}} state={skill} array={project.keyFeatures}/>
                 
 
                 <Heading color='#9DFFFF' fontFamily='Staatliches' fSize="22px" width="100%" padding="60px 0 0 0">Design and Development Process</Heading>
                 <iframe style={{border:"1px solid rgba(0, 0, 0, 0.1)", maxWidth:"800px", width:"100%"}}  height="450" src={project.figma} allowFullScreen></iframe>
+
+                <Heading color='#9DFFFF' fontFamily='Staatliches' fSize="22px" width="100%" padding="60px 0 0 0">Lessons Learned</Heading>
+                {/* <Text padding="20px 0 0 0">{project && project.lessonsLearned[1].title}</Text> */}
+
                 <Box padding="20px 0 80px 0" width="100%" justCont="center">
                     <a href={project.github}>
                         <Image alt="github image" className='linkGithub' src="/Github/github.png" width={50} height={50} priority />
