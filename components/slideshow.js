92d7eb983d0e71b104a77cc363c12ce37af1cd15
Ana Arango <anaarango@Anas-Macbook-Air.local>
2023-06-15 21:33:48 -0700

    fixing slideshow client-side error

--- components/slideshow.js
+++ components/slideshow.js
@@ -1,4 +1,4 @@
-// import Tech from '@/data/tools.json'
+import Tech from '@/data/tools.json'
 // import Image from "next/image"
 import { Box, Image } from "./globals"
 import { useEffect, useState } from 'react';
@@ -22,10 +22,10 @@ export default function TechSlideshow(){
     return(
         <Box className="slideshow" style={{position:"relative", overflow:"hidden", background:"linear-gradient(to right, rgba(163,153,226,0) 20%, rgba(163,153,226,1) 50%,rgba(163,153,226,0) 80%)", width:"100%"}}>
             <Box className='sheesh' style={{display:"flex", position:"relative", alignItems:"flex-start", justifyContent:"flex-start", padding:"10px"}}>
-              {imagesArray.map((o,i)=>(
+              {imagesArray && imagesArray.map((o,i)=>(
                 <Image key={i} title={o.slice(0, -4)} className="techLogo" src={`/HorizontalLogos/${o}`} alt={o} width={45} height={45} margin="0 25px"></Image>
               ))}
-              {imagesArray.map((o,i)=>(
+              {imagesArray && imagesArray.map((o,i)=>(
                   <Image key={i} title={o.slice(0, -4)} className="techLogo" src={`/HorizontalLogos/${o}`} alt={o} width={45} height={45} margin="0 25px"></Image>
               ))}
             </Box>
