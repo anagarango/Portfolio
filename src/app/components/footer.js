668e4b45131c96918ce6cd4802f17ef1d3f68061
Ana Arango <anitag.arango@icloud.com>
2023-08-14 16:12:22 -0700

    changed styling of project card for consistency

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -7,7 +7,7 @@ export default function Footer(){
     const Links = [
         {image:"Github/github", url:"https://github.com/anagarango"},
         {image:"LinkedIn", url:"https://www.linkedin.com/in/ana-arango-08592122a/"},
-        {image:"Email", url:"/#Contact"}
+        {image:"email", url:"/#Contact"}
     ]
     return(
         <Box position="fixed" left="0" bottom="0" height="35px" width="100vw" bgColor="#2B255B" aliIt="center" padding="20px 17px" justCont="space-between" zIndex="5" borderTop="3px solid #0a002c">
